7c2f9c94e27e937db8ea6c9d4078c48e455f5a2e
Talhabinfaisall <m.talhafaisal123@gmail.com>
2023-12-05 23:53:12 +0500

    New work

---
+++ new java project/app.js
@@ -0,0 +1,45 @@
+
+const taskInputForm = document.querySelector("#task-form")
+
+taskInputForm.addEventListener("submit", function (event){
+  event.preventDefault();
+  console.log(event, "form is submited");
+  const selectTaskInput = document.querySelector("#task");
+  const taskInputValue = selectTaskInput.value;
+  if(!taskInputValue){
+    alert("please fill the task input field");
+    return;
+  }
+
+  const collection = document.querySelector(".collection");
+
+  const createLiElement = document.createElement("li");
+  createLiElement.className = ".collection-item";
+  createLiElement.innerHTML = `${taskInputValue}
+                          <a href="#" class="delete-item secondary-content">
+                            <i class="fa fa-remove"></i>
+                          </a>`;
+  collection.append(createLiElement);
+  selectTaskInput.value = "";
+})
+
+const selectCollection = document.querySelector(".collection")
+
+selectCollection.addEventListener("click", function(e){
+  e.preventDefault();
+  if (event.target.className === "fa fa-remove"){
+    if (confirm("Are you sure?")){
+      event.target.parentElement.parentElement.remove();
+    }
+  }
+})
+
+const selectcleartaskbtn = document.querySelector(".clear-tasks");
+
+selectcleartaskbtn.addEventListener("click", function(e){
+  e.preventDefault();
+  if (confirm("Are you sure?")){
+    const collection = document.querySelector(".collection");
+    collection.innerHTML = "";
+  }
+})
